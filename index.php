56168cfe8b4e17f27239422a0d880f7110802a94
gnysek <524094+gnysek@users.noreply.github.com>
2015-02-17 15:16:17 +0100

    moved to glue framework, extended a little

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once('cfg.php');
+include_once('fetcher/gmapi-data.php');
+include_once('fetcher/glue.php');
+include_once('fetcher/controllers.php');
+include_once('fetcher/functions.php');
+
+$urls = array(
+	'/' => 'index',
+	'/api' => 'api',
+	'/version/([a-z4]+)' => 'version',
+);
+
+
+
+try {
+	glue::stick($urls);
+} catch (Exception $e) {
+	include('fetcher/design/header.php');
+	echo '404 - not found';
+	include('fetcher/design/footer.php');
+}
+
+
